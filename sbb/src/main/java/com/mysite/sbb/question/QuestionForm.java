43073d815872e834155e6bdb3e1718916a22879f
hajju0617 <joowon023617@gmail.com>
2024-12-28 23:56:01 +0900

    Spring boot board - 글(질문) 작성 기능 구현 중.

---
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb.question;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm {
+	@NotBlank(message = "제목은 필수 입력 항목.")		// NotEmpty (deprecated)
+	@Size(max=200)
+	private String subject;
+	
+	@NotBlank(message = "내용은 필수 입력 항목.")
+	private String content;
+}
